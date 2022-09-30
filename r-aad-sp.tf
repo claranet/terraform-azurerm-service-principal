21fa04937c8b0746f7d3ddde48af330b4e5c47b0
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-09-30 17:34:19 +0200

    AZ-854: Force `prevent_duplicate_names`

--- r-aad-sp.tf
+++ r-aad-sp.tf
@@ -2,11 +2,14 @@ resource "azuread_application" "aad_app" {
   display_name = var.sp_display_name
   owners       = var.sp_owners
   tags         = var.sp_aad_app_tags
+
+  prevent_duplicate_names = true
 }
 
 resource "azuread_service_principal" "sp" {
-  application_id               = azuread_application.aad_app.application_id
-  owners                       = var.sp_owners
+  application_id = azuread_application.aad_app.application_id
+  owners         = var.sp_owners
+
   app_role_assignment_required = false
 }
 
